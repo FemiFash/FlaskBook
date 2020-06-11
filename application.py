a176c68c32562f8181296411fc2ba0aec14cf288
FemiFash <fashipef@hotmail.com>
2020-06-11 09:50:57 +0100

    2nd Commit: First Blueprint

--- application.py
+++ application.py
@@ -5,4 +5,8 @@ def create_app():
 	app = Flask(__name__)
 	app.config.from_pyfile('settings.py')
 	# app.config.from_object('settings')
+	
+	from user.views import user_app  # registering blueprint app in central application.py file
+	app.register_blueprint(user_app) # ..... for app to be used in any other module
+	
 	return app
